f1072cc06b78bf48a23c5d98e7eadfe12ca4c9fc
Andrej Balaz <andrejbalaz001@gmail.com>
2023-02-16 17:45:31 +0100

    finished login script

--- .local/bin/login.sh
+++ .local/bin/login.sh
@@ -2,28 +2,18 @@
 
 set -euo pipefail
 
-# Lock      - i3lock -c 000000 -f
-# Hibernate - systemctl hibernate
-# Restart   - i3 restart
-# Exit      - i3-msg exit
-# Shutdown  - shutdown now
-# Reboot    - reboot
-
 option=$(
-    echo -e "Lock\nShutdown\nReboot" \
-    | dmenu -i -l 3 -fn "SourceCodeVariable-16"
+    echo -e "Lock\nReload\nExit\nHibernate\nShutdown\nReboot" \
+    | dmenu -i -l 6 -fn "SourceCodeVariable-16"
 )
 
-echo $option;
-
 case $option in
-    "Lock") echo "Locking...";;
-    "Shutdown") echo "Shutting down...";;
-    *) echo "Unknown option";;
+    "Lock")     i3lock -c 000000 -f;;
+    "Reload")   i3 restart;;
+    "Exit")     i3-msg exit;;
+    "Sleep")    systemctl hibernate;;
+    "Shutdown") shutdown now;;
+    "Reboot")   reboot;;
+    *)          i3-nagbar -m "Unknown option." -f "pango:SourceCodeVariable 16";;
 esac
-
-# dmenu calculator
-# press tab to continue with last answer
-# xsel -o | dmenu -p Calculate: | xargs echo | bc 2>&1 | dmenu -p Answer: | xsel -i
-
 
